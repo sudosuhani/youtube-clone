3886d28daf0147f81f3ef6f927a38693a0900a13
Suhani Sharma <232030087+sudosuhani@users.noreply.github.com>
2026-08-04 15:25:50 +0530

    Installed material UI & folder structure for home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Sidebar from './sidebar/Sidebar'
+import MainContainer from './mainContainer/MainContainer'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex'>
+      <div className='flex  px-4 bg-red-300'>
+        <Sidebar />
+      </div>
+      <div className='flex  px-4'>
+        <MainContainer />
+      </div>
+    </div>
   )
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,11 @@
+import Header from "./header/Header";
 import Home from "./Home";
+import MainContainer from "./mainContainer/MainContainer";
+import Sidebar from "./sidebar/Sidebar";
 
 export {
+    Sidebar,
+    MainContainer,
+    Header,
     Home
 }
